c7651ab67a65625c7e8c07ebb4d393f9de0b67a8
Mohammad Azam <azamsharp@gmail.com>
2017-10-26 15:32:05 -0500

    source code from arkit webinar

--- BuildingARApps/ViewController.swift
+++ BuildingARApps/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    private var planes :[Plane] = [Plane]()
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,14 +21,71 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        let sphere = SCNSphere(radius: 0.3)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "jupiter.jpg")!
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode(geometry: sphere)
+        node.position = SCNVector3(0,0,-0.5)
+        
+        let fishScene = SCNScene(named: "fish.dae")!
+        let fishNode = fishScene.rootNode.childNode(withName: "fishModel", recursively: true)!
+        fishNode.position = SCNVector3(0,0,-1.5)
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        //scene.rootNode.addChildNode(node)
+        
+       // scene.rootNode.addChildNode(fishNode)
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        registerGestureRecognizers()
+    }
+    
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitTestResult.isEmpty {
+            
+            guard let hitResult = hitTestResult.first else {
+                return
+            }
+            addTV(hitResult :hitResult)
+        }
+    }
+    
+    private func addTV(hitResult :ARHitTestResult) {
+        
+        let offsetY = Float(0.2)
+        
+        let tvScene = SCNScene(named: "tv.dae")!
+        let tvNode = tvScene.rootNode.childNode(withName: "tvNode", recursively: true)!
+        
+        tvNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,hitResult.worldTransform.columns.3.y + offsetY, hitResult.worldTransform.columns.3.z)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvNode)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,8 +94,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = Plane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+            }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,35 +130,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
